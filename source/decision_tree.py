009bcdc3e1b53b490fbb57c83eeca921dc149701
Yogarajalakshmi S <yogarajalakshmis@gmail.com>
2025-12-06 20:12:25 -0500

    basic filtering by signature size and security assumption

--- source/decision_tree.py
+++ source/decision_tree.py
@@ -19,3 +19,28 @@ class DecisionTree:
 
     def reset(self):
         self.current_candidates = self.schemes.copy()
+
+    def filter_by_signature_size(self, pref: str):
+        filtered = []
+        for s in self.current_candidates:
+            raw = s["Signature Size (bits)"]
+            parts = [int(x) for x in raw.replace("-", ",").split(",")]
+            avg = (min(parts) + max(parts)) // 2
+
+            if pref == "short" and avg <= 512:
+                filtered.append(s)
+            elif pref == "medium" and 512 < avg <= 2048:
+                filtered.append(s)
+            elif pref == "long" and avg > 2048:
+                filtered.append(s)
+
+        self.current_candidates = filtered
+
+
+    def filter_by_security_assumption(self, assumption: str):
+        filtered = []
+        for s in self.current_candidates:
+            if assumption.lower() in s["Security Assumption"].lower():
+                filtered.append(s)
+
+        self.current_candidates = filtered
